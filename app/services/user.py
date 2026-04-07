f2c9a0cd24745ef75801f2a1e1f0b1607b30d372
Víctor Rivas <VictorRivas0230@gmail.com>
2026-04-07 15:56:30 -0400

    [FIX] Fix UserService initialization by correctly calling the superclass constructor

--- app/services/user.py
+++ app/services/user.py
@@ -6,4 +6,4 @@ from app.services.base import BaseService
 
 class UserService(BaseService[User]):
     def __init__(self, db: AsyncSession):
-        return await super().update(user_id, user)
+        super().__init__(db, User)
